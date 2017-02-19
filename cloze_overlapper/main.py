ee6d987f91cc509b3625e7b87a3c5e5b62123876
Glutanimate <glutanimate@users.noreply.github.com>
2017-02-19 20:22:14 +0100

    Start implementing new cloze generation logic

--- cloze_overlapper/main.py
+++ cloze_overlapper/main.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
-Anki Add-on: Editor Overlapping Clozes
+This file is part of the Cloze Overlapper add-on for Anki
 
-Copyright: (c) Glutanimate 2016-2017
+Copyright: Glutanimate 2016-2017
 License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-
 """
 
 import sys
@@ -19,7 +18,11 @@ from anki.hooks import addHook
 overlapping_cloze_note_type = u"E-Lückentext-OL-ns"
 overlapping_cloze_fields_prefix = "Text"
 overlapping_cloze_fields_fullcloze = "Full"
-overlapping_cloze_max_cards = 15
+
+ol_cloze_max = 20
+ol_cloze_no_context_first = False
+ol_cloze_no_context_last = False
+ol_cloze_incremental_ends = False
 
 def generateOverlappingClozes(self, text):
     lines = text.splitlines()
@@ -28,7 +31,7 @@ def generateOverlappingClozes(self, text):
     htmlBefore = """<div><ol type="1" start="1" style="margin-left: 20px; ">"""
     htmlAfter = """</ol></div>"""
     # regular cloze fields
-    for idx in range(0, overlapping_cloze_max_cards):
+    for idx in range(0, ol_cloze_max):
         if idx >= total:
             # delete contents of unused fields
             fields.append("")
@@ -67,6 +70,61 @@ def generateOverlappingClozes(self, text):
 
     return {'fields': fields, 'fullCloze': fullCloze}
 
+def getClozeStart(idx, target, total):
+    if idx < target or idx > total:
+        return 0
+    return idx-target
+
+def getBeforeStart(idx, target, total, start_c):
+    if start_c < 1 or (ol_cloze_no_context_last and idx == total):
+        return None
+    if target == "a" or target > start_c:
+        return 0
+    return start_c-target
+
+def getAfterEnd(idx, target, total):
+    left = total - idx
+    if left < 1 or (ol_cloze_no_context_first and idx == 1):
+        return None
+    if target == "a" or target > left:
+        return total
+    return idx+target
+
+def generateClozes(self, text, options):
+    before, prompt, after = options
+    print options
+    lines = unicode(text, "utf-8").splitlines()
+    length = len(lines)
+    if ol_cloze_incremental_ends:
+        total = length + prompt - 1
+        start = 1
+    else:
+        total = length - prompt
+        start = prompt
+    if total > ol_cloze_max:
+        print "Error: more clozes than the note type can handle"
+        return
+    fields = []
+    cloze_format = u"{{c%i::%s}}"
+    for idx in range(start,total+1):
+        field = ["..."] * total
+        start_c = getClozeStart(idx, prompt, total)
+        start_b = getBeforeStart(idx, before, total, start_c)
+        end_a = getAfterEnd(idx, after, total)
+        print start_b, start_c, end_a
+        if start_b is not None:
+            field[start_b:start_c] = lines[start_b:start_c]
+        field[start_c:idx] = [cloze_format % (idx, l) for l in lines[start_c:idx]]
+        if end_a is not None:
+            field[idx:end_a] = lines[idx:end_a]
+        fields.append(field)
+    if ol_cloze_max > total:
+        # delete contents of unused fields
+        fields = fields + [""] * (ol_cloze_max - total)
+    full = [cloze_format % (ol_cloze_max+1, l) for l in lines]
+
+    return fields, full
+
 def insertOverlappingCloze(self):
     # make sure the right model is set
     modelName = self.note.model()["name"]
@@ -83,9 +141,9 @@ def insertOverlappingCloze(self):
         return False
 
     # make sure the right number of lines are selected
-    if not 2 <= selection.count("\n") <= overlapping_cloze_max_cards-1:
+    if not 2 <= selection.count("\n") <= ol_cloze_max-1:
         tooltip("Please select between 3 and %d lines" 
-            % overlapping_cloze_max_cards )
+            % ol_cloze_max )
         return False
 
     retDict = self.generateOverlappingClozes(selection)
