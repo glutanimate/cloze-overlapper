a937804e6df19b4ab109c4bdb6061a79cd834093
Glutanimate <glutanimate@users.noreply.github.com>
2017-02-19 20:21:36 +0100

    Add template module

---
+++ cloze_overlapper/template.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of the Cloze Overlapper add-on for Anki
+
+Copyright: Glutanimate 2016-2017
+License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+"""
+
