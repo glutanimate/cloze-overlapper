33532ea87707d1cf8439a07b26ba496e9911ef09
Glutanimate <glutanimate@users.noreply.github.com>
2019-01-25 23:20:00 +0100

    Update source code headers. Prepare anki20 compat with future imports.

--- src/cloze_overlapper/config.py
+++ src/cloze_overlapper/config.py
@@ -1,13 +1,42 @@
 # -*- coding: utf-8 -*-
 
-"""
-This file is part of the Cloze Overlapper add-on for Anki
+# Cloze Overlapper Add-on for Anki
+#
+# Copyright (C) 2016-2019  Aristotelis P. <https://glutanimate.com/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version, with the additions
+# listed at the end of the accompanied license file.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# NOTE: This program is subject to certain additional terms pursuant to
+# Section 7 of the GNU Affero General Public License.  You should have
+# received a copy of these additional terms immediately following the
+# terms and conditions of the GNU Affero General Public License which
+# accompanied this program.
+#
+# If not, please request a copy through one of the means of contact
+# listed here: <https://glutanimate.com/contact/>.
+#
+# Any modifications to this file must keep this entire header intact.
 
-Configuration
-
-Copyright: Glutanimate 2016-2018
-License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.html
 """
+Handles add-on configuration
+
+FIXME: temporary holdover until libaddon is ready for generalized use
+"""
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
 
 import os
 
--- src/cloze_overlapper/overlapper.py
+++ src/cloze_overlapper/overlapper.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
 
+# Cloze Overlapper Add-on for Anki
+#
+# Copyright (C)  2016-2019 Aristotelis P. <https://glutanimate.com/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version, with the additions
+# listed at the end of the accompanied license file.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# NOTE: This program is subject to certain additional terms pursuant to
+# Section 7 of the GNU Affero General Public License.  You should have
+# received a copy of these additional terms immediately following the
+# terms and conditions of the GNU Affero General Public License which
+# accompanied this program.
+#
+# If not, please request a copy through one of the means of contact
+# listed here: <https://glutanimate.com/contact/>.
+#
+# Any modifications to this file must keep this entire header intact.
+
 """
-This file is part of the Cloze Overlapper add-on for Anki
-
-Overlapping Cloze Adder
-
-Copyright: Glutanimate 2016-2018
-License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.html
+Adds overlapping clozes
 """
 
 from .consts import *
