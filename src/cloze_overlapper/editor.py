7377b7b91708cba14a402674d823d322810a9f97
newageoflight <camint3rnal@live.com>
2020-12-10 22:34:53 +1100

    Fixes silly mistake

--- src/cloze_overlapper/editor.py
+++ src/cloze_overlapper/editor.py
@@ -291,7 +291,7 @@ def onAddCards(self, _old):
     editor = self.editor
     note = editor.note
     showInfo("Called from onAddCards: "+str(note))
-    showInfo("Model check: "+str(checkModel(note.model()), notify=False))
+    showInfo("Model check: "+str(checkModel(note.model(), notify=False)))
 
     if not note or not checkModel(note.model(), notify=False):
         return _old(self)
